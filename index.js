433ca1053f680ba8a1d3a54497e97143d2d4e73e
maurostein <maurostein@gmail.com>
2021-10-25 14:22:40 +0200

    prompt testen

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const prompt = require('prompt-sync')();
 
 console.log("Geben Sie ein Wert für a");
 const a = Number (prompt('a? '));
-// tem que dar o valor de a no terminal
+// muss den Wert von a im Terminal angeben
 
 console.log("Geben Sie jetzt ein Wert für b");
 const b = Number(prompt('b? '));
-// colocar novamente o valor de b no terminal
+// Gib den Wert von b zurück in das Terminal
 
 console.log("Die Summe von a + b:");
 console.log(a + b);
