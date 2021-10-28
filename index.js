2a9fb998de984b0a82c392e71fab51cd8b008694
maurostein <maurostein@gmail.com>
2021-10-28 11:16:09 +0200

    text verändern

--- index.js
+++ index.js
@@ -290,11 +290,11 @@ while (true ) {
         Gib nur A, B oder C) \n`);
   }
 }
-name = prompt(`Wie heißt deine Tier?`);
+name = prompt(`Wie heißt dein Tier?`);
 console.log(` 
             \\o/
              I
-            / \\  yuhuuuu du hast jetzt ein ${type} 
+            / \\  Yuhuuu du hast jetzt ein ${type} 
             `);
 const tier = new Pet(name, type);
 console.log(tier);
@@ -327,7 +327,7 @@ while(exit=='n' && !tier.gesundheit<=0){
         tier.feed('apfel')
         tier.printStatus()
        }else{
-           console.log(`Du hast falsche buchstabe gegeben`);
+           console.log(`Du hast falsche Buchstabe gegeben`);
        }
    }else if(ergebniss.toLowerCase()==='c'){
     
@@ -347,13 +347,13 @@ while(exit=='n' && !tier.gesundheit<=0){
     tier.printStatus()
    }
    else{
-       console.log(`Du hast falsche buchstabe gegeben`);
+       console.log(`Du hast falsche Buchstabe gegeben`);
    }
 }
 console.log(`GAME OVER ${tier.name} ist leider gestorben`);
 
 const hund = new Pet('Mami', 'hunde')
-
+/* 
 hund.feed('apfel');
 console.log(hund);
 hund.play()
@@ -413,7 +413,7 @@ hund.sleep()
 hund.sleep()
 hund.sleep()
 hund.sleep()
-console.log(hund);
+console.log(hund); */
 
 
 
@@ -437,4 +437,4 @@ const kartoffel= new Food('kartoffel', 3)
 const fisch= new Food('fisch', 3)
 const carotte= new Food('carotte', 2)
 const fleisch= new Food('fleisch', 3)
-
+*/
